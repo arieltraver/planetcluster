a2f28ad84e0ae004ef85291c1c323aa234445a83
arieltraver <arieltraver@users.noreply.github.com>
2023-05-14 17:11:13 -0400

    todo

--- starter.py
+++ starter.py
@@ -8,6 +8,7 @@ from sklearn.mixture import GaussianMixture
 from matplotlib import pyplot
 # define dataset
 X, _ = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
+# TODO HERE: transform the planet data from a csv into something usable here.
 # define the model
 model = GaussianMixture(n_components=2)
 # fit the model
@@ -29,6 +30,7 @@ pyplot.show()
 #DBSCAN
 # define dataset
 X, _ = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
+# TODO HERE: transform the planet data from a csv into something usable here.
 # define the model
 model = DBSCAN(eps=0.30, min_samples=9)
 # fit model and predict clusters
