1b8532d72e3aae26dfea8e1efd4a3371e5377965
arieltraver <arieltraver@users.noreply.github.com>
2023-05-15 04:34:35 -0400

    starter working amazing

--- csv2dataframe.py
+++ csv2dataframe.py
@@ -1,3 +1,9 @@
 import os
 import pandas as pd
 import numpy as np
+
+def create_frame(filename):
+    frame = pd.read_csv(filename)
+    return frame
+
+
--- starter.py
+++ starter.py
@@ -7,6 +7,7 @@ from numpy import unique
 from numpy import where
 from sklearn.datasets import make_classification
 from sklearn.mixture import GaussianMixture
+#from sklearn.mixture import DBScan
 from matplotlib import pyplot
 import os
 import pandas as pd
@@ -18,27 +19,28 @@ def create_frame(filename):
 
 # define dataset
 df = create_frame("metal_vs_density.csv")
-print(df.to_string())
-X, _ = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
+df2 = df.drop(["pl_denserr1","pl_denserr2","pl_denslim","st_meterr1","st_meterr2","st_metlim"],axis=1)
+df2 = df2.dropna().reset_index()
 # TODO HERE: transform the planet data from a csv into something usable here.
 # define the model
 model = GaussianMixture(n_components=2)
 # fit the model
-model.fit(X)
+model.fit(df2)
 # assign a cluster to each example
-yhat = model.predict(X)
+yhat = model.predict(df2)
 # retrieve unique clusters
 clusters = unique(yhat)
 # create scatter plot for samples from each cluster
 for cluster in clusters:
-	# get row indexes for samples with this cluster
-	row_ix = where(yhat == cluster)
-	# create scatter of these samples
-	pyplot.scatter(X[row_ix, 0], X[row_ix, 1])
+    # get row indexes for samples with this cluster
+    row_ix = where(yhat == cluster)
+    df_filtered = df2.drop(row_ix[0])
+    # create scatter of these samples
+    pyplot.scatter(df_filtered["pl_dens"], df_filtered["st_met"])
 # show the plot
 pyplot.show()
 
-
+'''
 #DBSCAN
 # define dataset
 X, _ = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
@@ -56,3 +58,4 @@ for cluster in clusters:
  # create scatter of these samples
  pyplot.scatter(X[row_ix, 0], X[row_ix, 1])
 # show the plot
+'''
